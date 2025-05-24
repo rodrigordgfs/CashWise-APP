7e3127f6427c2a67026f33d665da0250a47087cc
Rodrigo Shinoda <32286127+rodrigordgfs@users.noreply.github.com>
2025-05-23 22:08:45 -0300

    Fix React Hydration Mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
-import { ReactNode } from "react";
 import { MenuProvider } from "@/context/menuContext";
 import { ThemeProvider } from "@/context/darkModeContext";
 import { SidebarProvider } from "@/context/sidebarContext";
@@ -17,30 +15,22 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: ReactNode;
+  children: React.ReactNode;
 }>) {
   return (
-    <ThemeProvider>
-      <MenuProvider>
-        <SidebarProvider>
-          <html lang="pt-BR" className="h-full">
-            <body
-              className={`${inter.className} h-full bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100`}
-            >
+    <html lang="pt-BR" className="h-full" suppressHydrationWarning>
+      <body
+        className={`${inter.className} h-full bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100`}
+        suppressHydrationWarning
+      >
+        <ThemeProvider>
+          <MenuProvider>
+            <SidebarProvider>
               {children}
-              <Script id="theme-switcher">
-                {`
-          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-          } else {
-            document.documentElement.classList.remove('dark')
-          }
-          `}
-              </Script>
-            </body>
-          </html>
-        </SidebarProvider>
-      </MenuProvider>
-    </ThemeProvider>
+            </SidebarProvider>
+          </MenuProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
