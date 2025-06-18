c6e22eb797ac56c0ce21dd777a61ca62a45bacdf
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-06-18 11:43:25 -0300

    feature: Adicionado o Componente Table do package shinodalabs-ui

--- src/components/ui/transactions/TransactionTable/index.tsx
+++ src/components/ui/transactions/TransactionTable/index.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { EmptyState } from "@/components/shared/EmptyState";
-import { TransactionTableHeader } from "./TransactionTableHeader";
-import { TransactionTableRow } from "./TransactionTableRow";
 import { Transaction } from "@/types/Transaction.type";
 import { useTranslation } from "react-i18next";
-import { Pagination } from "shinodalabs-ui";
+import { Pagination, Table } from "shinodalabs-ui";
 import { useTransaction } from "@/context/transactionsContext";
+import { useCategory } from "@/context/categoryContext";
+import { formatCurrency } from "@/utils/formatConvertCurrency";
+import { useSettings } from "@/context/settingsContext";
+
+interface HeaderColumn<T> {
+  key: keyof T | "actions";
+  label: React.ReactNode;
+  align?: "left" | "right" | "center";
+  hidden?: boolean;
+  render?: (value: T[keyof T], row: T) => React.ReactNode;
+  format?: (value: T[keyof T], row: T) => React.ReactNode;
+  style?: (value: T[keyof T], row: T) => string;
+  showMobile?: boolean;
+}
 
 interface TransactionTableProps {
   transactions: Transaction[];
@@ -20,8 +32,68 @@ export const TransactionTable = ({
   onClickEdit,
 }: TransactionTableProps) => {
   const { t } = useTranslation();
+  const { categories } = useCategory();
+  const { currency, language } = useSettings();
   const { page, setPage, totalItems, totalPages, setPerPage, perPage } =
     useTransaction();
+
+  const columns: HeaderColumn<Transaction>[] = [
+    {
+      key: "description",
+      label: t("transactions.description"),
+    },
+    {
+      key: "category",
+      label: t("transactions.category"),
+      format: (_value, row) =>
+        categories.find((c) => c.id === row.category?.id)?.name ?? "-",
+    },
+    {
+      key: "date",
+      label: t("transactions.date"),
+      format: (value) => {
+        if (
+          typeof value === "string" ||
+          typeof value === "number" ||
+          value instanceof Date
+        ) {
+          return new Date(value).toLocaleDateString("pt-BR", {
+            timeZone: "UTC",
+          });
+        }
+        return "-";
+      },
+    },
+    {
+      key: "account",
+      label: t("transactions.account"),
+      format: (_value, row) => row.account ?? "-",
+    },
+    {
+      key: "paid",
+      label: t("transactions.paid"),
+      align: "right",
+      format: (value) => (value ? "✅" : "❌"),
+    },
+    {
+      key: "amount",
+      label: t("transactions.amount"),
+      align: "right",
+      format: (value) => {
+        if (typeof value === "number") {
+          return formatCurrency(Math.abs(value), currency, language);
+        }
+        return formatCurrency(0, currency, language);
+      },
+      style: (_value, row) =>
+        row.type === "INCOME" ? "text-green-500" : "text-red-500",
+    },
+    {
+      key: "actions",
+      label: t("transactions.actions"),
+      align: "right",
+    },
+  ];
 
   if (transactions.length === 0) {
     return (
@@ -33,31 +105,28 @@ export const TransactionTable = ({
   }
 
   return (
-    <div className="rounded-lg border border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950 shadow-sm">
-      <div className="p-6">
-        <div className="overflow-x-auto">
-          <table className="w-full">
-            <TransactionTableHeader />
-            <tbody>
-              {transactions.map((transaction) => (
-                <TransactionTableRow
-                  key={transaction.id}
-                  transaction={transaction}
-                  onClickEdit={onClickEdit}
-                  onClickDelete={onClickDelete}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div className="flex flex-col gap-4">
+      <Table>
+        <Table.Header columns={columns} />
+        <Table.Body>
+          {transactions.map((t) => (
+            <Table.Row
+              key={t.id}
+              data={t}
+              columns={columns}
+              onClickEdit={() => onClickEdit?.(t)}
+              onClickDelete={() => onClickDelete?.(t)}
+            />
+          ))}
+        </Table.Body>
+      </Table>
       <Pagination
         currentPage={page}
         totalPages={totalPages}
         totalItems={totalItems}
         perPage={perPage}
-        onPageChange={(page) => setPage(page)}
-        onItemsPerPageChange={(perPage) => setPerPage(perPage)}
+        onPageChange={setPage}
+        onItemsPerPageChange={setPerPage}
         labels={{
           previous: t("app.pagination.previous"),
           next: t("app.pagination.next"),
