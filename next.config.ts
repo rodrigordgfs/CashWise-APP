c004e1ef13883d46a85780ddf089505773fcab7a
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-06-18 14:05:01 -0300

    fix: Ajustes no NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
+  // assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
   experimental: {
     serverActions: {}, // se você realmente usa serverActions, coloque true aqui
   },
