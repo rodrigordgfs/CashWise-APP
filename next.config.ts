4ed19ccb6815aeb87bddab4ee451df20041c932e
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-06-18 20:41:59 -0300

    fix: Ajuste no nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  assetPrefix: "https://cdn.appcashwise.com.br",
   experimental: {
     serverActions: {}, // se você realmente usa serverActions, coloque true aqui
   },
